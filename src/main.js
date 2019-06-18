e3417d6bd49e2e36a6c08a7662c7f91c562815ef
chenhe <chenhe_Ashley@163.com>
2019-06-18 19:47:33 +0800

    init project

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// 入口文件
+import Vue from 'vue'
+
+// 按需导入 Mint-UI 中的组件
+import {Header} from 'mint-ui'
+Vue.component(Header.name, Header)
+
+// 导入 MUI 的样式表
+import './lib/mui/css/mui.min.css'
+
+// 导入 App 根组件
+import App from './App.vue'
+
+
+var vm = new Vue({
+    el: '#app',
+    data: {},
+    methods: {},
+    render: c => c(App)
+});
